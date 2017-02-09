46679808071e33cb482648b0cd13a79ef460e557
Leonard <leonardquintal@gmail.com>
2017-02-09 17:11:59 -0500

    mostly done

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,11 +14,17 @@ public class GameController : MonoBehaviour {
 
     ///References:
     public Slider _TenstionSlider;
+    
     public GameObject _Pick;
+    private GameObject m_LockHolder;
+    private List<TumblerPin> m_Cylinders = new List<TumblerPin>();
 
     /// <summary>
     /// Designed Variables
     /// </summary>
+    //Easy, normal, hard, impossible values
+    [SerializeField]
+    float[] m_Diff_TensionRange = new float[4]{ 8f, 4f, 2f, 1f };
     //Easy, normal, hard, impossible values
     [SerializeField]
     float[] m_Diff_Tension = new float[4]{ 1f, 2f, 4f, 8f };
@@ -26,23 +32,55 @@ public class GameController : MonoBehaviour {
     Vector4 m_Pick_Bounds = new Vector4();
     [SerializeField]
     float m_Pick_MovementSpeed = 0.5f;
+    [SerializeField]
+    float m_LowChance = 0.5f;
+    [SerializeField]
+    float m_HighChance = 5f;
+    [SerializeField]
+    float m_ChanceRate = 0.5f;
 
     private float m_MouseInput = 0f;
     private bool m_Lifting = false;
     private float m_AppliedTension = 0f;
+    private bool m_IsGameOver = false;
+    private float m_CurrentTension = 0f;
+    private float m_ChanceTimer = 0f;
 
 	// Use this for initialization
 	void Start () {
+        m_LockHolder = GameObject.FindGameObjectWithTag("Lock");
+        foreach (Transform child in m_LockHolder.transform)
+        {
+            if (child.GetComponent<TumblerPin>())
+            {
+                m_Cylinders.Add(child.GetComponent<TumblerPin>());
+            }
+        }
+        //float sliderSize = _TenstionSlider.GetComponent<RectTransform>().sizeDelta.y;
+        //_TenstionSlider.transform.FindChild("break").GetComponent<RectTransform>().sizeDelta =  new Vector2(1f, -(sliderSize - (sliderSize / 10f * m_Diff_TensionRange[(int)m_Difficulty])));
+        //_TenstionSlider.transform.FindChild("high").GetComponent<RectTransform>().sizeDelta =   new Vector2(1f, -(sliderSize - (sliderSize / 10f * m_Diff_TensionRange[(int)m_Difficulty])));
+        //_TenstionSlider.transform.FindChild("low").GetComponent<RectTransform>().sizeDelta =    new Vector2(1f, -(sliderSize - (sliderSize / 10f * m_Diff_TensionRange[(int)m_Difficulty])));
+        //_TenstionSlider.transform.FindChild("none").GetComponent<RectTransform>().sizeDelta =   new Vector2(1f, -(sliderSize - (sliderSize / 10f * m_Diff_TensionRange[(int)m_Difficulty]))); 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        PlayerInput();        
+        if (m_IsGameOver)
+        {
+            return;
+        }
+        PlayerInput();
+        CheckGameOver();      
 	}
 
     void FixedUpdate()
     {
+        if (m_IsGameOver)
+        {
+            return;
+        }
         CalculateTension();
+        CheckTension();
         MovePick();
     }
 
@@ -82,6 +120,99 @@ public class GameController : MonoBehaviour {
     void CalculateTension()
     {
         _TenstionSlider.value = Mathf.Clamp01(_TenstionSlider.value += m_AppliedTension * Time.deltaTime);
-       
+        m_CurrentTension = _TenstionSlider.value;
+    }
+
+    void CheckTension()
+    {
+        Debug.Log(m_CurrentTension);
+        if (m_CurrentTension > 0.9f)
+        {
+            //break the tool
+            CheckToolBreakage(100f);
+        }
+        else if (m_CurrentTension > 0.75f)
+        {
+            //high chance of break
+            if (Time.time > m_ChanceTimer)
+            {
+                m_ChanceTimer = Time.time + m_ChanceRate;
+                CheckToolBreakage(m_HighChance * m_Diff_Tension[(int)m_Difficulty]);
+            }
+        }
+        else if (m_CurrentTension > 0.6f)
+        {
+            //Small break chance
+            if (Time.time > m_ChanceTimer)
+            {
+                m_ChanceTimer = Time.time + m_ChanceRate;
+                CheckToolBreakage(m_LowChance * m_Diff_Tension[(int)m_Difficulty]);
+            }
+            
+        }
+        else if (m_CurrentTension < 0.4f)
+        {
+            //small chance pin go down
+            if (Time.time > m_ChanceTimer)
+            {
+                m_ChanceTimer = Time.time + m_ChanceRate;
+                CheckPinDown(m_LowChance * m_Diff_Tension[(int)m_Difficulty]);
+            }
+        }
+        else if (m_CurrentTension < 0.25f)
+        {
+            // high change pin go down
+            if (Time.time > m_ChanceTimer)
+            {
+                m_ChanceTimer = Time.time + m_ChanceRate;
+                CheckPinDown(m_HighChance * m_Diff_Tension[(int)m_Difficulty]);
+            }
+        }
+        else if(m_CurrentTension < 0.1f)
+        {
+            //pins all go down
+            CheckPinDown(100f);
+        }
+    }
+
+    void CheckToolBreakage(float chance)
+    {
+        if (Random.Range(0f, 100f) > chance)
+        {
+            return;
+        }
+        Debug.Log("Tools broke");
+        m_IsGameOver = true;
+    }
+
+    void CheckPinDown(float chance)
+    {
+        foreach (TumblerPin tp in m_Cylinders)
+        {
+            if (tp.m_IsInPosition)
+            {
+                if (Random.Range(0f, 100f) < chance)
+                {
+                    tp.DropDown();
+                    Debug.Log("Pin out of position");
+                }
+            }
+        } 
+        
+        
+    }
+
+    void CheckGameOver()
+    {
+        
+        foreach (TumblerPin tp in m_Cylinders)
+        {
+            if (!tp.m_IsInPosition)
+            {
+                return;
+            }
+        }
+        m_IsGameOver = true;
+        Debug.Log("GameOver");
     }
 }
---
+++ Assets/Scripts/TumblerPin.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumblerPin : MonoBehaviour {
+
+    /// <summary>
+    /// References
+    /// </summary>
+    [SerializeField]
+    GameObject _TopCylinder;
+    [SerializeField]
+    Material _CorrectMat;
+    [SerializeField]
+    Material _WrongMat;
+
+    //The sweet spot we are trying to reach.
+    [SerializeField]
+    float _Target_Y;
+    //The max distance the tumbler can go up.
+    [SerializeField]
+    float _Max_Y;
+    //Upward movespeed
+    [SerializeField]
+    float _Up_Speed;
+    //Downward movespeed
+    [SerializeField]
+    float _Down_Speed;
+    [SerializeField]
+    float _Pin_DeadZone = 0.02f;
+    
+
+    //The lower resting place of the tumbler.
+    private float m_Starting_Y;
+    //Tracks if the pick is touching the cylinder.
+    private bool m_IsPicking = false;
+    public bool m_IsInPosition = false;
+    private Rigidbody m_Rb;
+
+    // Use this for initialization
+    void Start () {
+        m_Starting_Y = transform.position.y;
+        _Target_Y = _Target_Y - GetComponent<Collider>().bounds.extents.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Abs(transform.position.y - _Target_Y) < _Pin_DeadZone)
+        {
+            if (!m_IsInPosition && m_IsPicking)
+            {
+                m_IsInPosition = true;
+                _TopCylinder.GetComponent<Renderer>().material = _CorrectMat;
+            }
+            
+        }
+        else
+        {
+            if (m_IsInPosition)
+            {
+                m_IsInPosition = false;
+                _TopCylinder.GetComponent<Renderer>().material = _WrongMat;
+            }
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (!m_IsPicking && transform.position.y > m_Starting_Y && !m_IsInPosition)
+        {
+            transform.Translate(Vector3.down * _Down_Speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_IsPicking = true;
+            if (transform.position.y < _Max_Y)
+            {
+                transform.Translate(Vector3.up * _Up_Speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_IsPicking = false;
+        }
+        
+    }
+
+    public void DropDown()
+    {
+        m_IsInPosition = false;
+        _TopCylinder.GetComponent<Renderer>().material = _WrongMat;
+    }
+}
